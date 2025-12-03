17f9315236d50a719a41c46b0de97c5317ced910
fractioneater <fractioneater@gmail.com>
2025-12-03 18:13:29 -0500

    Day 3 more elegantly

--- day3.cpp
+++ day3.cpp
@@ -25,18 +25,18 @@ int main() {
 
 #   elif PART == 2
 
-    auto a { std::max_element(line.begin(), line.end() - 11) };                                      // 1
-    auto b { std::max_element(line.begin() + 1 + std::distance(line.begin(), a), line.end() - 10) }; // 2
-    auto c { std::max_element(line.begin() + 1 + std::distance(line.begin(), b), line.end() - 9) };  // 3
-    auto d { std::max_element(line.begin() + 1 + std::distance(line.begin(), c), line.end() - 8) };  // 4
-    auto e { std::max_element(line.begin() + 1 + std::distance(line.begin(), d), line.end() - 7) };  // 5
-    auto f { std::max_element(line.begin() + 1 + std::distance(line.begin(), e), line.end() - 6) };  // 6
-    auto g { std::max_element(line.begin() + 1 + std::distance(line.begin(), f), line.end() - 5) };  // 7
-    auto h { std::max_element(line.begin() + 1 + std::distance(line.begin(), g), line.end() - 4) };  // 8
-    auto i { std::max_element(line.begin() + 1 + std::distance(line.begin(), h), line.end() - 3) };  // 9
-    auto j { std::max_element(line.begin() + 1 + std::distance(line.begin(), i), line.end() - 2) };  // 10
-    auto k { std::max_element(line.begin() + 1 + std::distance(line.begin(), j), line.end() - 1) };  // 11
-    auto l { std::max_element(line.begin() + 1 + std::distance(line.begin(), k), line.end()) };      // 12
+    auto a { std::max_element(line.begin(), line.end() - 11) }; // 1
+    auto b { std::max_element(a + 1, line.end() - 10) };        // 2
+    auto c { std::max_element(b + 1, line.end() - 9) };         // 3
+    auto d { std::max_element(c + 1, line.end() - 8) };         // 4
+    auto e { std::max_element(d + 1, line.end() - 7) };         // 5
+    auto f { std::max_element(e + 1, line.end() - 6) };         // 6
+    auto g { std::max_element(f + 1, line.end() - 5) };         // 7
+    auto h { std::max_element(g + 1, line.end() - 4) };         // 8
+    auto i { std::max_element(h + 1, line.end() - 3) };         // 9
+    auto j { std::max_element(i + 1, line.end() - 2) };         // 10
+    auto k { std::max_element(j + 1, line.end() - 1) };         // 11
+    auto l { std::max_element(k + 1, line.end()) };             // 12
 
 #   if VERBOSE
     std::cout << *a << *b << *c << *d << *e << *f << *g << *h << *i << *j << *k << *l << '\n';
