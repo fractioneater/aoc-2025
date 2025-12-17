3805f11a6ae8f2677b9ae05c1458c07bd42d20c4
fractioneater <fractioneater@gmail.com>
2025-12-17 16:25:25 -0500

    Day 12

--- day12.cpp
+++ day12.cpp
@@ -1,33 +1,51 @@
 #include "h/day12.h"
 
+#include <cmath>
 #include <iostream>
+#include <sstream>
 
 #include "file.h"
 
-#define VERBOSE 1
-
-namespace Day12 {
-  long p1() {
-    return 0;
-  }
-
-  long p2() {
-    return 0;
-  }
-}
+#define VERBOSE 1}
 
 int day12(File& in) {
-  using namespace Day12;
+  // This solution is going to be HEAVILY optimized to my input.
 
-  // PLAN: ...
-    
+  int blanks {};
   std::string line {};
-  while (in.next_line(line)) {}
+  while (in.next_line(line)) {
+    if (line.empty()) blanks++;
+    if (blanks == 6) break;
+  }
 
-  const long sol1 { p1() };
-  std::cout << "PART 1: Answer " << sol1 << "\n";
-  // const long sol2 { p2() };
-  // std::cout << "PART 2: Answer " << sol2 << "\n";
+  long sol {};
+  while (in.next_line(line)) {
+    std::istringstream iss { line };
+    long x {};
+    char c {};
+    long y {};
+    iss >> x >> c >> y;
+    std::cout << x << " * " << y;
+
+    long piece_count {};
+    int n {};
+    iss.get();
+    while (iss >> std::ws >> n)
+      piece_count += n;
+
+    std::cout << ": " << piece_count << " pieces\n";
+
+    // This condition makes more sense: (piece_count <= std::floor(x / 3.0) * std::floor(y / 3.0))
+    // But it turns out all I need is this:
+    if (piece_count * 9 <= x * y) {
+      std::cout << "It works!\n";
+      sol++;
+    } else {
+      std::cout << "Nope\n";
+    }
+  }
+
+  std::cout << sol << " of them work\n";
 
   return 0;
 }
